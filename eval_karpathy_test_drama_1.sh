8da44e136e8bf5c844cf687e7f008483db85e36a
fanjiaqi <117280641+fjq-tongji@users.noreply.github.com>
2024-12-23 10:58:51 +0800

    Update eval_karpathy_test_drama_1.sh

--- eval_karpathy_test_drama_1.sh
+++ eval_karpathy_test_drama_1.sh
@@ -1,7 +1,7 @@
 python eval_drama_1.py --dump_images 0  \
 	--num_images 2544  \
-        --infos_path /data/fjq/drama_log/log_transformer_drama_cap_blip2_27b/infos_transformer_blip2.pkl \
-	--model /data/fjq/drama_log/log_transformer_drama_cap_blip2_27b/model.pth \
+        --infos_path /data/fjq/drama_log/log_transformer_drama_cap/infos_transformer.pkl \
+	--model /data/fjq/drama_log/log_transformer_drama_cap/model.pth \
 	--language_eval 1  \
 	--batch_size 10   \
 	--split test       \
@@ -9,6 +9,6 @@ python eval_drama_1.py --dump_images 0  \
 	--temperature 0.1  \
 	--top_p 0.75  \
 	--drama_img_path  /data/fjq/DRAMA/imgs/   \
-	--save_bbox_img_path  /data/fjq/drama_log/log_transformer_drama_cap_blip2_27b/regs/   \
-	--save_path_seq /data/fjq/drama_log/log_transformer_drama_cap_blip2_27b/eval_karpathy_test_seq_drama.json \
-	--save_path_index_iou /data/fjq/drama_log/log_transformer_drama_cap_blip2_27b/eval_karpathy_test_index_iou_drama.json  \
+	--save_bbox_img_path  /data/fjq/drama_log/log_transformer_drama_cap/regs/   \
+	--save_path_seq /data/fjq/drama_log/log_transformer_drama_cap/eval_karpathy_test_seq_drama.json \
+	--save_path_index_iou /data/fjq/drama_log/log_transformer_drama_cap/eval_karpathy_test_index_iou_drama.json  \
