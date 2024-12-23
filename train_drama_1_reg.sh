53bd35ff49bfa445be3e78610ae90b0ec56b1494
fanjiaqi <117280641+fjq-tongji@users.noreply.github.com>
2024-12-23 10:36:32 +0800

    Update and rename train_drama_1.sh to train_drama_1_reg.sh

--- train_drama_1_reg.sh
+++ train_drama_1_reg.sh
@@ -1,11 +1,11 @@
-id="transformer_drama_cap_blip2_27b"
+id="transformer_drama_reg"
 if [ ! -f log/log_$id/infos_$id.pkl ]; then
 start_from=""
 else
 start_from="--start_from log/log_$id"
 fi
-python train_drama_1-cap-blip2.py --id transformer_blip2  \
-    --caption_model transformer_blip2 \
+python train_drama_1-reg.py --id transformer  \
+    --caption_model transformer \
     --refine 1 \
     --refine_aoa 1 \
     --use_ff 0 \
@@ -19,8 +19,8 @@ python train_drama_1-cap-blip2.py --id transformer_blip2  \
     --label_smoothing 0.2 \
     --pretrained_llama /data/fjq/LLAMA/llama-2-7b/consolidated.00_1.pth  \
     --pretrained_reg  /data/fjq/drama_log/log_transformer_0304_swin-b/model-stage1.pth       \
-    --input_json data/drama/blip2/drama_dataset/drama_llama_adapter_DRAMA.json \
-    --input_label_h5 data/drama/blip2/drama_dataset/drama_llama_adapter_DRAMA.h5 \
+    --input_json data/drama/drama_llama_adapter_3_r4_0211.json \
+    --input_label_h5 data/drama/drama_llama_adapter_3_r4_0211.h5 \
     --h5_reg  True   \
     --input_att_dir  /data/fjq/DRAMA/att  \
     --input_grid_dir  /data/fjq/DRAMA/imgs_features_res101_224 \
@@ -29,7 +29,7 @@ python train_drama_1-cap-blip2.py --id transformer_blip2  \
     --batch_size 64  \
     --beam_size 2  \
     --use_box   0  \
-    --learning_rate 1e-4 \
+    --learning_rate 1e-5 \
     --num_layers 6 \
     --input_encoding_size 512 \
     --rnn_size 2048 \
@@ -38,10 +38,10 @@ python train_drama_1-cap-blip2.py --id transformer_blip2  \
     --scheduled_sampling_start 0 \
     --checkpoint_path /data/fjq/drama_log/log_$id  \
     $start_from   \
-    --save_checkpoint_every 300  \
+    --save_checkpoint_every 3000  \
     --language_eval 1 \
     --val_images_use -1 \
-    --max_epochs 5 \
+    --max_epochs 150 \
     --save_history_ckpt 0   \
     --scheduled_sampling_increase_every 5 \
     --scheduled_sampling_max_prob 0.5 \
